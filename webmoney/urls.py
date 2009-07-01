6399712f855f0aa75d89d6f728691f0edd466d8f
Ivan Fedorov <oxyum@oxyum.ru>
2009-07-01 14:56:29 +0400

    small fix - remove junk string.

--- webmoney/urls.py
+++ webmoney/urls.py
@@ -11,8 +11,6 @@ urlpatterns = patterns('',
     url(r'^result/$', views.result, name='webmoney-result'),
 )
 
-if not hasattr(settings, ''):
-
 if getattr(settings, 'MERCHANT_WM_USE_SIMPLE_PAYMENT', False):
     urlpatterns += patterns('',
         url(r'^$', views.simple_payment, name='simple_payment'),
